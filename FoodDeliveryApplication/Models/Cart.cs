96ad1d7699814018cb8d56fe3b9e9000b0d7b55d
Raksha Ganyarpawar <raksha_ganyarpawar@persistent.com>
2022-11-09 14:47:37 +0530

    This the first commit

---
+++ FoodDeliveryApplication/Models/Cart.cs
@@ -0,0 +1,38 @@
+﻿namespace FoodDeliveryApplication.Models
+{
+    public class Cart
+    {
+        public string UserName { get; set; }
+
+        public string FoodItem { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string FoodImage { get; set; }
+
+        public int Price { get; set; }
+
+        public int FoodId { get; set; }
+
+        public int RestaurantId { get; set; }
+
+        //public string RestaurantName { get; set; }
+
+        public Cart()
+        {
+
+        }
+
+        public Cart(string userName, string foodItem, int quantity, string foodImage, int price, int foodId, int restaurantId)
+        {
+            this.UserName = userName;
+            this.FoodItem = foodItem;
+            this.Quantity = quantity;
+            this.FoodImage = foodImage;
+            this.Price = price;
+            this.FoodId = foodId;
+            this.RestaurantId = restaurantId;
+            //this.RestaurantName = restaurantName;
+        }
+    }
+}
